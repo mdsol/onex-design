038b8741d6f0da8124904d2cff89fe5cae97ce8c
ekabanchenko-mdsol <ekabanchenko@mdsol.com>
2022-02-28 20:59:12 +0200

    added tests for dropdown, lozenges, navigation

--- src/tests/components.test.js
+++ src/tests/components.test.js
@@ -8,7 +8,6 @@ describe('components render', () => {
   });
 
   it('avatar', async () => {
-    console.log('process', process.env.STYLED);
     const url = process.env.STYLED
       ? 'http://localhost:3000/?component=Avatar&styled=true'
       : 'http://localhost:3000/?component=Avatar&styled=false';
@@ -23,11 +22,72 @@ describe('components render', () => {
     expect(image).toMatchImageSnapshot();
   });
 
+  it('badge', async () => {
+    const url = process.env.STYLED
+      ? 'http://localhost:3000/?component=Badge&styled=true'
+      : 'http://localhost:3000/?component=Badge&styled=false';
+    const page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: 'networkidle2',
+    });
+    await page.waitForSelector('#app > div > div.wrapper');
+    const element = await page.$('#app > div > div.wrapper');
+    const image = await element.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
   it('buttons', async () => {
-    console.log('process', process.env.STYLED);
     const url = process.env.STYLED
       ? 'http://localhost:3000/?component=Button&styled=true'
       : 'http://localhost:3000/?component=Button&styled=false';
+    const page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: 'networkidle2',
+    });
+    await page.waitForSelector('#app > div > div.wrapper');
+    const element = await page.$('#app > div > div.wrapper');
+    const image = await element.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('dropdown', async () => {
+    const url = process.env.STYLED
+      ? 'http://localhost:3000/?component=Dropdown&styled=true'
+      : 'http://localhost:3000/?component=Dropdown&styled=false';
+    const page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: 'networkidle2',
+    });
+    await page.waitForSelector('#app > div > div.wrapper');
+    const element = await page.$('#app > div > div.wrapper');
+    const image = await element.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('lozenge', async () => {
+    console.log('process', process.env.STYLED);
+    const url = process.env.STYLED
+      ? 'http://localhost:3000/?component=Lozenge&styled=true'
+      : 'http://localhost:3000/?component=Lozenge&styled=false';
+    const page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: 'networkidle2',
+    });
+    await page.waitForSelector('#app > div > div.wrapper');
+    const element = await page.$('#app > div > div.wrapper');
+    const image = await element.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('navigation', async () => {
+    console.log('process', process.env.STYLED);
+    const url = process.env.STYLED
+      ? 'http://localhost:3000/?component=Navigation&styled=true'
+      : 'http://localhost:3000/?component=Navigation&styled=false';
     const page = await browser.newPage();
     await page.goto(url, {
       waitUntil: 'networkidle2',
--- src/view-components/components-config.js
+++ src/view-components/components-config.js
@@ -1,4 +1,21 @@
-import { PlusCircleFill } from 'react-bootstrap-icons';
+import { PlusCircleFill, ThreeDotsVertical } from 'react-bootstrap-icons';
+
+const dropdownItems = [
+  { eventKey: 'one', title: 'item one' },
+  { eventKey: 'two', title: 'item two' },
+  { eventKey: 'three', title: 'item three' },
+  { eventKey: 'four', title: 'item four' },
+];
+
+const tabsVisibleItems = [
+  { eventKey: 'one', title: 'item one' },
+  { eventKey: 'two', title: 'item two' },
+  { eventKey: 'three', title: 'item three' },
+  { eventKey: 'four', title: 'item four' },
+];
+
+const tabsHiddenItems = [{ eventKey: 'five', title: 'item five' }];
+
 // eslint-disable-next-line import/prefer-default-export
 export const config = {
   data: {
@@ -36,6 +53,66 @@ export const config = {
           {
             src: 'https://robohash.org/my-own-slug.png?size=300x300&set=set1',
             size: 'xxl',
+          },
+        ],
+      },
+      {
+        name: 'Badge',
+        componentName: 'Badge',
+        variants: [
+          {
+            type: 'primary',
+            size: 'sm',
+            children: '100',
+            className: 'custom-badge',
+            dataTestId: 'text-badge-id',
+          },
+          {
+            type: 'important',
+            size: 'sm',
+            children: '100',
+            className: 'custom-badge',
+            dataTestId: 'text-badge-id',
+          },
+          {
+            type: 'default',
+            size: 'sm',
+            children: '100',
+          },
+          {
+            type: 'default-invert',
+            size: 'sm',
+            children: '100',
+            className: 'custom-badge',
+            dataTestId: 'text-badge-id',
+          },
+          {
+            type: 'removed',
+            size: 'sm',
+            children: '100-',
+            className: 'custom-badge',
+            dataTestId: 'text-badge-id',
+          },
+          {
+            type: 'added',
+            size: 'sm',
+            children: '+100',
+            className: 'custom-badge',
+            dataTestId: 'text-badge-id',
+          },
+          {
+            type: 'primary',
+            size: 'sm',
+            children: '100',
+            className: 'custom-badge',
+            dataTestId: 'text-badge-id',
+          },
+          {
+            type: 'primary',
+            size: 'lg',
+            children: '100',
+            className: 'custom-badge',
+            dataTestId: 'text-badge-id',
           },
         ],
       },
@@ -108,6 +185,175 @@ export const config = {
           {
             variant: 'link',
             children: <PlusCircleFill />,
+          },
+        ],
+      },
+      {
+        name: 'Dropdown',
+        componentName: 'Dropdown',
+        variants: [
+          {
+            title: 'Small dropdown',
+            id: 'small-dropdown',
+            items: dropdownItems,
+            size: 'sm',
+          },
+          {
+            title: 'Large dropdown',
+            id: 'large-dropdown',
+            items: dropdownItems,
+            size: 'lg',
+          },
+          {
+            title: 'Primary dropdown',
+            id: 'primary-dropdown',
+            items: dropdownItems,
+          },
+          {
+            variant: 'secondary',
+            title: 'Secondary dropdown',
+            id: 'secondary-dropdown',
+            items: dropdownItems,
+          },
+          {
+            variant: 'link',
+            title: 'Tertiary dropdown',
+            id: 'tertiary-dropdown',
+            items: dropdownItems,
+          },
+          {
+            title: 'Disabled primary dropdown',
+            id: 'primary-disabled-dropdown',
+            items: dropdownItems,
+            disabled: true,
+          },
+          {
+            variant: 'secondary',
+            title: 'Disabled secondary dropdown',
+            id: 'secondary-disabled-dropdown',
+            items: dropdownItems,
+            disabled: true,
+          },
+          {
+            variant: 'link',
+            title: 'Disabled tertiary dropdown',
+            id: 'tertiary-disabled-dropdown',
+            items: dropdownItems,
+            disabled: true,
+          },
+          {
+            title: <ThreeDotsVertical />,
+            id: 'primary-icon-dropdown',
+            items: dropdownItems,
+            buttonStyle: 'icon',
+          },
+          {
+            variant: 'secondary',
+            title: <ThreeDotsVertical />,
+            id: 'secondary-icon-dropdown',
+            items: dropdownItems,
+            buttonStyle: 'icon',
+          },
+          {
+            variant: 'icon-link',
+            title: <ThreeDotsVertical />,
+            id: 'tertiary-icon-dropdown',
+            items: dropdownItems,
+            buttonStyle: 'icon',
+          },
+        ],
+      },
+      {
+        name: 'Lozenge',
+        componentName: 'Lozenge',
+        variants: [
+          {
+            variant: 'subtle',
+            type: 'success',
+            size: 'lg',
+            children: 'Success',
+          },
+          {
+            variant: 'bold',
+            type: 'success',
+            size: 'lg',
+            children: 'Success',
+          },
+          {
+            variant: 'icon',
+            type: 'success',
+            icon: <PlusCircleFill />,
+            size: 'lg',
+            children: 'Success',
+          },
+          {
+            variant: 'subtle',
+            type: 'warning',
+            size: 'lg',
+            children: 'Warning',
+          },
+          {
+            variant: 'bold',
+            type: 'warning',
+            size: 'lg',
+            children: 'Warning',
+          },
+          {
+            variant: 'icon',
+            type: 'warning',
+            icon: <PlusCircleFill />,
+            size: 'lg',
+            children: 'Warning',
+          },
+          {
+            variant: 'subtle',
+            type: 'error',
+            size: 'lg',
+            children: 'Error',
+          },
+          {
+            variant: 'bold',
+            type: 'error',
+            size: 'lg',
+            children: 'Error',
+          },
+          {
+            variant: 'icon',
+            type: 'error',
+            icon: <PlusCircleFill />,
+            size: 'lg',
+            children: 'Error',
+          },
+          {
+            variant: 'subtle',
+            type: 'info',
+            size: 'lg',
+            children: 'Info',
+          },
+          {
+            variant: 'bold',
+            type: 'info',
+            size: 'lg',
+            children: 'Info',
+          },
+          {
+            variant: 'icon',
+            type: 'info',
+            icon: <PlusCircleFill />,
+            size: 'lg',
+            children: 'Info',
+          },
+        ],
+      },
+      {
+        name: 'Navigation',
+        componentName: 'Navigation',
+        variants: [
+          {
+            id: 'custom-navigation',
+            visibleItems: tabsVisibleItems,
+            hiddenItems: tabsHiddenItems,
+            defaultActiveKey: 'three',
           },
         ],
       },
