b47f4a81afa21c7674de80791fa91229c13bcdf8
Yuliia Dementsova <ydementsova@mdsol.com>
2022-03-14 14:04:36 +0200

    added style for counter type of budget

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,31 @@
 import { useState, useEffect } from 'react';
-import { Badge, Button } from 'react-bootstrap';
-import { Badge as SoloBadge, Button as SoloButton, Tabs } from './components';
+import { Tabs, Badge } from './components';
 import './scss/platform.scss';
-import { StyledAvatar, StyledBadge, StyledButtons } from './styled';
 import ViewComponents from './view-components';
-
-const props = {
-  type: 'important',
-  size: 'sm',
-  children: '100',
-  className: 'custom-badge',
-  dataTestId: 'text-badge-id',
-};
+import { CameraFillIcon } from './icons';
 
 const DevExamples = () => (
   <div className="p-5">
-    <h2>Global CSS</h2>
-    <p>
-      <Button size="sm">Bootstrap Button</Button>
-    </p>
-    <p>
-      <SoloButton>Onex Button</SoloButton>
-    </p>
-    <p>
-      <Badge>10</Badge>
-    </p>
-    <p>
-      <SoloBadge type="primary">10</SoloBadge>
-    </p>
-    <h2>Scoped CSS</h2>
-    <p>
-      <StyledButtons>Button</StyledButtons>
-    </p>
-    <p>
-      <StyledAvatar />
-    </p>
-    <p>
-      <StyledBadge {...props} />
-    </p>
+    <div>
+      <Badge iconStart={<CameraFillIcon />} type="default" variant="counter">
+        100
+      </Badge>
+      <Badge iconStart={<CameraFillIcon />} type="primary" variant="counter">
+        100
+      </Badge>
+      <Badge iconStart={<CameraFillIcon />} type="important" variant="counter">
+        100
+      </Badge>
+      <Badge iconStart={<CameraFillIcon />} type="removed" variant="counter">
+        100
+      </Badge>
+      <Badge iconStart={<CameraFillIcon />} type="added" variant="counter">
+        100
+      </Badge>
+      <Badge type="default-invert" variant="counter" iconEnd={<CameraFillIcon />}>
+        100
+      </Badge>
+    </div>
   </div>
 );
 
