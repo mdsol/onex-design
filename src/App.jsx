a3138c3ed74ad05b0795188ca7c40898de86efba
Yuliia Dementsova <ydementsova@mdsol.com>
2022-03-14 16:07:35 +0200

    added icon statys type of badget

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,35 @@ import { useState, useEffect } from 'react';
 import { Tabs, Badge } from './components';
 import './scss/platform.scss';
 import ViewComponents from './view-components';
-import { CameraFillIcon } from './icons';
+import { InfoRoundFillIcon } from './icons';
+import LockIcon from '@mui/icons-material/Lock';
 
 const DevExamples = () => (
   <div className="p-5">
     <div>
-      <Badge type="default" variant="status">
-        Default
+      <Badge type="default" variant="status-icon">
+        <LockIcon />
       </Badge>
-      <Badge type="info" variant="status">
-        Information
+    </div>
+    <div>
+      <Badge type="primary" variant="counter">
+        100
       </Badge>
+    </div>
+    <div>
       <Badge type="important" variant="status">
-        Error
+        Important
       </Badge>
-      <Badge type="success" variant="status">
+    </div>
+    <div>
+      <Badge type="success" variant="status-bold">
         Success
       </Badge>
-      <Badge type="warning" variant="status">
-        Worning
+    </div>
+    <div>
+      <Badge type="added" variant="counter">
+        <LockIcon />
+        Test
       </Badge>
     </div>
   </div>
