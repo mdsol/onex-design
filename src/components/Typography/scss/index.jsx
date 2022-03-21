b242833ce1130caceb6bc3eb5aea24bc756e8342
ekabanchenko-mdsol <ekabanchenko@mdsol.com>
2022-03-21 13:25:24 +0200

    fixed Typography

--- src/components/Typography/scss/index.jsx
+++ src/components/Typography/scss/index.jsx
@@ -37,9 +37,10 @@ const Typography = ({
     },
   );
 
-  const TypographyComponent = href
-    ? 'a'
-    : (paragraph ? 'p' : defaultVariantMapping[variant]) || 'span';
+  const TypographyComponent =
+    href && variant.search(/<\/?h[1-5]>/gi) !== -1
+      ? 'a'
+      : (paragraph ? 'p' : defaultVariantMapping[variant]) || 'span';
 
   return (
     <TypographyComponent data-test-id={dataTestId} className={typographyClassNames} href>
