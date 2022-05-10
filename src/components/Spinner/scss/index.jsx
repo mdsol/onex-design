4596a722db406c7f79a63440aaae020e39e717d4
adatsenko-mdsol <adatsenko@mdsol.com>
2022-05-10 12:04:56 +0300

    update components for equal linter ruels

--- src/components/Spinner/scss/index.jsx
+++ src/components/Spinner/scss/index.jsx
@@ -1,27 +1,29 @@
 import PropTypes from 'prop-types';
-import classNames from "classnames";
-import { Spinner } from "react-bootstrap";
+import classNames from 'classnames';
+import { Spinner } from 'react-bootstrap';
 
-const DefaultSpinner = ({className ,...props}) => {
-    const classes = classNames('onex-spinner', `onex-spinner--${props.size}`,{
-        [className]: className,
-    });
+const DefaultSpinner = ({ className, ...props }) => {
+  const classes = classNames('onex-spinner', `onex-spinner--${props.size}`, {
+    [className]: className,
+  });
 
-    return (
-        <div className={classes}>
-            <Spinner {...props} variant='primary'/>
-        </div>
-    )
+  return (
+    <div className={classes}>
+      <Spinner {...props} variant="primary" />
+    </div>
+  );
 };
 
 DefaultSpinner.propTypes = {
-    animation: PropTypes.oneOf('border', 'grow'),
-    size: PropTypes.oneOf('sm', 'md', 'lg'),
+  animation: PropTypes.oneOf('border', 'grow'),
+  size: PropTypes.oneOf('sm', 'md', 'lg'),
+  className: PropTypes.string,
 };
 
 DefaultSpinner.defaultProps = {
-    animation: 'border',
-    size: 'md',
+  animation: 'border',
+  size: 'md',
+  className: '',
 };
 
 export default DefaultSpinner;
