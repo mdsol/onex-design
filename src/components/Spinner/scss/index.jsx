2f71bd712efc7e7c2835fdbf5637be9af9e1de98
adatsenko-mdsol <adatsenko@mdsol.com>
2022-05-12 22:30:43 +0300

    fixes for spinner, offcanvas and progressbar

--- src/components/Spinner/scss/index.jsx
+++ src/components/Spinner/scss/index.jsx
@@ -9,19 +9,17 @@ const DefaultSpinner = ({ className, ...props }) => {
 
   return (
     <div className={classes}>
-      <Spinner {...props} variant="primary" />
+      <Spinner {...props} variant="primary" animation="border" />
     </div>
   );
 };
 
 DefaultSpinner.propTypes = {
-  animation: PropTypes.oneOf(['border', 'grow']),
   size: PropTypes.oneOf(['sm', 'md', 'lg']),
   className: PropTypes.string,
 };
 
 DefaultSpinner.defaultProps = {
-  animation: 'border',
   size: 'md',
   className: '',
 };
--- src/stories/Onex/components/Spinner/Spinner.stories.jsx
+++ src/stories/Onex/components/Spinner/Spinner.stories.jsx
@@ -4,19 +4,6 @@ export default {
   title: 'Onex/Components/Spinner',
   component: Spinner,
   argTypes: {
-    animation: {
-      description: 'The visual color style of the spinner',
-      options: ['border', 'grow'],
-      control: { type: 'select' },
-      table: {
-        defaultValue: {
-          summary: 'border',
-        },
-        type: {
-          summary: 'grow | border',
-        },
-      },
-    },
     size: {
       description: 'Component size variations',
       options: ['sm', 'md', 'lg'],
@@ -29,7 +16,6 @@ export default {
           summary: 'sm | md | lg',
         },
       },
-      
     },
   },
 };
@@ -38,26 +24,14 @@ const Template = (args) => <Spinner {...args} />;
 
 export const BorderAnimation = Template.bind({});
 
-BorderAnimation.args = {
-  animation: 'border',
-};
-
-export const GrowAnimation = Template.bind({});
-
-GrowAnimation.args = {
-  animation: 'grow',
-};
-
 export const Small = Template.bind({});
 
 Small.args = {
-  animation: 'border',
   size: 'sm',
 };
 
 export const Large = Template.bind({});
 
 Large.args = {
-  animation: 'border',
   size: 'lg',
 };
