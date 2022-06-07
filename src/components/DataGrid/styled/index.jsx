2ca96c6ad72804ee659abaa089fdb944944a8547
adatsenko-mdsol <adatsenko@mdsol.com>
2022-06-07 04:20:58 +0300

    small fixes for segmented toggle, tabs and alerts

--- src/components/DataGrid/styled/index.jsx
+++ src/components/DataGrid/styled/index.jsx
@@ -1,5 +1,14 @@
-import StyledDataGrid from './components/dataGrid';
-import StyledDataGridControl from './components/dataGridControl';
-import StyledDataGridName from './components/dataGridName';
+import styled from 'styled-components';
+import DataGrid from '../scss';
+// eslint-disable-next-line import/no-unresolved
+import cssString from './platform.css?raw';
 
-export { StyledDataGrid, StyledDataGridControl, StyledDataGridName };
+// eslint-disable-next-line react/prop-types
+export default ({ children, ...props }) => <StyledComponent {...props}>{children}</StyledComponent>;
+
+// eslint-disable-next-line prefer-template
+const handleCssString = (str) => '&' + str;
+
+const StyledComponent = styled(DataGrid)`
+  ${() => cssString.replaceAll(/\.onex-dataGrid.[^__]+?[.|\s]/g, handleCssString)}
+`;
