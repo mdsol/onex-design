e87aa97dcd3913dd9ba57e2f9c635674d0ea11ce
Elena Kabachenko <ekabanchenko@mdsol.com>
2022-06-30 03:12:12 +0300

    fixed dataGrid stories

--- src/stories/Onex/components/DataGrid/gridData.jsx
+++ src/stories/Onex/components/DataGrid/gridData.jsx
@@ -1,4 +1,4 @@
-import { Button, Icon, Dropdown, Badge } from '../../../../components';
+import { Button, Icon, Dropdown, Badge, Tag } from '../../../../components';
 
 export const dropdownItems = [
   { eventKey: 'one', title: 'Option 1' },
@@ -232,6 +232,110 @@ export const demoGridData = [
       ),
     },
     phase: 10,
+  },
+];
+
+export const demoCellGridColumns = [
+  {
+    Header: 'Study Id',
+    accessor: 'studyId',
+  },
+  {
+    Header: 'Study Name',
+    accessor: 'studyName',
+    type: 'custom',
+  },
+  {
+    Header: 'Stage',
+    accessor: 'stage',
+    type: 'custom',
+  },
+  {
+    Header: 'Status',
+    accessor: 'status',
+    type: 'editable',
+  },
+  {
+    Header: 'Phase',
+    accessor: 'phase',
+  },
+  {
+    accessor: 'action',
+    Header: (
+      <Button variant="tertiary" type="icon">
+        <Icon>settings</Icon>
+      </Button>
+    ),
+    Cell: ({ row }) => <Dropdown variant="tertiary" buttonStyle="icon" items={dropdownItems} />,
+    type: 'action',
+  },
+];
+
+export const demoCellGridData = [
+  {
+    studyId: 'A195800',
+    studyName: {
+      value: 'Study name 1',
+      leadingIcon: <Icon>check_circle</Icon>,
+      trailingIcon: <Icon>info</Icon>,
+    },
+    stage: {
+      component: <Tag>Stage 1</Tag>,
+    },
+    status: 'Status 1',
+    phase: 1,
+  },
+  {
+    studyId: 'A195801',
+    studyName: {
+      value: 'Study name 2',
+      leadingIcon: <Icon>check_circle</Icon>,
+      trailingIcon: <Icon>info</Icon>,
+    },
+    stage: {
+      component: <Tag>Stage 2</Tag>,
+    },
+    status: 'Status 2',
+    phase: 2,
+  },
+  {
+    studyId: 'A195802',
+    studyName: {
+      value: 'Study name 3',
+      leadingIcon: <Icon>check_circle</Icon>,
+      trailingIcon: <Icon>info</Icon>,
+    },
+    stage: {
+      component: <Tag>Stage 3</Tag>,
+    },
+    status: 'Status 3',
+    phase: 3,
+  },
+  {
+    studyId: 'A195803',
+    studyName: {
+      value: 'Study name 4',
+      leadingIcon: <Icon>check_circle</Icon>,
+      trailingIcon: <Icon>info</Icon>,
+    },
+    stage: {
+      component: <Tag>Stage 4</Tag>,
+    },
+    status: 'Status 4',
+    phase: 4,
+  },
+  {
+    studyId: 'A195804',
+    studyName: {
+      value: 'Study name 5',
+      leadingIcon: <Icon>check_circle</Icon>,
+      trailingIcon: <Icon>info</Icon>,
+    },
+    stage: {
+      component: <Tag>Stage 5</Tag>,
+    },
+    status: 'Status 5',
+    phase: 5,
   },
 ];
 
